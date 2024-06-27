df9c0e5d695b088f7d1268d4a8bf19f971461cf7
amine <bachar321amineo@gmail.com>
2024-06-27 08:04:44 +0200

    adding deployment requirement 4

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-python install_dependencies.py
+python dependance.py
 
 # Convert static asset files
 python manage.py collectstatic --no-input
