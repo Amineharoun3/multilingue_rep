271a2f33210a1f564849954879bdd191317791ef
amine <bachar321amineo@gmail.com>
2024-06-28 06:06:25 +0200

    fixing apturl error9

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 set -o errexit
 
 # Upgrade pip, setuptools, and wheel
-pip install --upgrade pip setuptools==59.6.0 wheel==0.37.0
+pip install --upgrade pip setuptools==59.6.0 wheel==0.37.0 PyYAML==5.4.1
 
 # Install Cython first
 pip install Cython==3.0.10
