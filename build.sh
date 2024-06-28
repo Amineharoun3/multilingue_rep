bc4edbdb4ef86e73635b7c4ec3174ba71e0efb74
amine <bachar321amineo@gmail.com>
2024-06-28 05:00:43 +0200

    fixing apturl error9

--- build.sh
+++ build.sh
@@ -9,7 +9,6 @@ set -o errexit
 # Update pip, setuptools, and wheel
 pip install --upgrade pip setuptools==59.6.0 wheel==0.37.0
 pip install Cython==3.0.10
-pip install PyYAML==5.4.1
 pip install -r requirements.txt
 
 # Convert static asset files
