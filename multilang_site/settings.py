09f55e830f5896707a4fa0bec5d0cda72b7d3374
amine <bachar321amineo@gmail.com>
2024-06-28 04:44:57 +0200

    fixing apturl error9

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -87,10 +87,7 @@ WSGI_APPLICATION = 'multilang_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///' + str(BASE_DIR / "db.sqlite3"))
 }
 
 
