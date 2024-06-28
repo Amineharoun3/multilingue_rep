a50e8cb08cc44010d393762697d1cab1a525e96e
amine <bachar321amineo@gmail.com>
2024-06-28 05:12:05 +0200

    fixing apturl error9

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@
 set -o errexit
 
 
-apt-get update
-&& apt-get install -y libsystemd-dev pkg-config build-essential
+apt-get update && apt-get install -y libsystemd-dev pkg-config build-essential
+
 # Update pip, setuptools, and wheel
 pip install --upgrade pip setuptools==59.6.0 wheel==0.37.0
 pip install Cython==3.0.10
+
 pip install -r requirements.txt
 
 # Convert static asset files
