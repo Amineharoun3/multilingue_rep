f8fc8ab27aa2a226854367b1c6e8dc2ab27d00c6
amine <bachar321amineo@gmail.com>
2024-06-29 11:34:11 +0200

    fixing virtual environment

--- build.sh
+++ build.sh
@@ -7,12 +7,13 @@ set -o errexit
 
 sudo apt-get update && sudo apt-get install -y apturl catfish command-not-found\
  cupshelpers lightdm-gtk-greeter-settings menulibre build-essential python3-dev libdbus-1-dev libcups2-dev \
-libgirepository1.0-dev build-essential libsystemd-dev pkg-config libcairo2-dev
+libgirepository1.0-dev build-essential libsystemd-dev pkg-config libcairo2-dev libyaml-dev
 
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 #python dependance.py
-pip install --upgrade pip
+pip install --upgrade pip setuptools wheel
+
 pip install "cython<3.0.0"
 pip install --no-build-isolation pyyaml==5.4.1
 pip install -r requirements.txt
