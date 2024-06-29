14879a841197b62e686502ecb444938d55abea3f
amine <bachar321amineo@gmail.com>
2024-06-29 13:19:43 +0200

    fixing virtual environment1

--- multilang_site/settings.py
+++ multilang_site/settings.py
@@ -87,7 +87,8 @@ WSGI_APPLICATION = 'multilang_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config('DATABASE_URL')}
+    'default': dj_database_url.config(default='postgresql://sqlite:SHJnx93KQzRGhlQjG1PgGoUAoiln1YOt@dpg-cpufjbmehbks73eg87n0-a/sqlite_d4bp')
+    }
 
 
 # Password validation
